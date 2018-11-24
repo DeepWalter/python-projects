d986b0423a7fcc0bbda841d79a990030ff586622
DeepWalter <deepwalter.cn@gmail.com>
2018-11-25 02:02:56 +0800

    Add bullets that can be fired by ship.

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -2,8 +2,10 @@ import sys
 
 import pygame
 
+from bullet import Bullet
 
-def check_keydown_event(event, ship):
+
+def check_keydown_event(event, ai_settings, screen, ship, bullets):
     """Respond to key presses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -13,6 +15,10 @@ def check_keydown_event(event, ship):
         ship.moving_up = True
     elif event.key == pygame.K_DOWN:
         ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_event(event, ship):
@@ -27,21 +33,25 @@ def check_keyup_event(event, ship):
         ship.moving_down = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_event(event, ship)
+            check_keydown_event(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_event(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_color)
+    # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    # Redraw the ship.
     ship.blitme()
 
     # Make the most recently drawn screen visible.
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -15,7 +15,7 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.centery = self.screen_rect.centery
+        self.rect.bottom = self.screen_rect.bottom
 
         # Store decimal values for the ship's center.
         self.centerx = float(self.rect.centerx)
