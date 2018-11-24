852eff40fb7d66e95eb3d3ce1fb497eb43d9c057
DeepWalter <deepwalter.cn@gmail.com>
2018-11-25 01:18:41 +0800

    Refactoring.

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -3,8 +3,46 @@ import sys
 import pygame
 
 
-def check_events():
+def check_keydown_event(event, ship):
+    """Respond to key presses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
+
+def check_keyup_event(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+
+def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_event(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_event(event, ship)
+
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship:
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship and get its rect.
         self.image = pygame.image.load('./images/ship.bmp')
@@ -14,8 +15,44 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centery = self.screen_rect.centery
+
+        # Store decimal values for the ship's center.
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
+
+        # Movement flag.
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship's position according to the movement flag."""
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.centerx += self.ai_settings.ship_speed_factor
+        # not elif since moveing_left and the rest are not mutually exclusive
+        if self.moving_left and self.rect.left > self.screen_rect.left:
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.centery -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
+        # Always make sure that the whole ship is in the screen
+        if self.rect.right > self.screen_rect.right:
+            self.rect.right = self.screen_rect.right
+        if self.rect.left < self.screen_rect.left:
+            self.rect.left = self.screen_rect.left
+        if self.rect.top < self.screen_rect.top:
+            self.rect.top = self.screen_rect.top
+        if self.rect.bottom > self.screen_rect.bottom:
+            self.rect.bottom = self.screen_rect.bottom
