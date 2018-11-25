881b357cef17a2c348e436b512764cd1672bd1fe
DeepWalter <deepwalter.cn@gmail.com>
2018-11-25 10:42:59 +0800

    Add an alien.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -21,13 +22,16 @@ def run_game():
     bullets = Group()
     # count_bullets = gf.bullets_counter(bullets)
 
+    # Make an alien.
+    alien = Alien(ai_settings, screen)
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
         # count_bullets()
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 if __name__ == '__main__':
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,8 +4,8 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings.
-        self.screen_width = 600
-        self.screen_height = 400
+        self.screen_width = 900
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
 
