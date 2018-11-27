c4993a7561000e1e01b0a69a6df9505c897654c0
DeepWalter <deepwalter.cn@gmail.com>
2018-11-27 10:52:54 +0800

    Make the play button clickable.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -32,7 +32,7 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, stats, screen, ship, bullets)
+        gf.check_events(ai_settings, stats, screen, play_button, ship, bullets)
 
         if stats.game_active:
             ship.update()
