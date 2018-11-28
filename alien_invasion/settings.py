0d65a202b81779a448bdb574b61b7e10351197b7
DeepWalter <deepwalter.cn@gmail.com>
2018-11-28 15:05:10 +0800

    Update docstring for Settings class.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,5 +1,48 @@
 class Settings:
-    """A class to store all settings for alien invasion."""
+    """A class to store all settings for alien invasion.
+
+    Attributes
+    ----------
+    screen_width: int
+        Width of the screen
+    screen_height: int
+        Height of the screen
+    bg_color: tuple
+        RGB color of the background of the screen
+    ship_limit: int
+        Maximum number of the ships
+    bullet_width: int
+        Width of the bullet
+    bullet_height: int
+        Height of the bullet
+    bullet_color: tuple
+        RGB color of the bullet
+    bullet_max_num: int
+        Maximum number of bullets on the screen
+    fleet_drop_speed: float
+        Speed of the fleet dropping down
+    speedup_scale: float
+        Game speedup scale
+    score_scale: float
+        Score speedup scale
+    ship_speed_factor: float
+        Speed of the ship
+    bullet_speed_factor: float
+        Speed of the bullet
+    alien_speed_factor: float
+        Speed of the bullet
+    fleet_direction: int
+        Direction of the fleet movement. 1 represents right; -1 left
+    alien_points: int
+        Points rewarded for each alien being shot
+
+    Methods
+    -------
+    initialize_dynamic_settings()
+        Initialize settings that change throughout the game
+    increase_speed()
+        Increase speed settings and alien point values
+    """
 
     def __init__(self):
         """Initialize the game's static settings."""
@@ -12,7 +55,7 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings.
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullet_max_num = 3
@@ -21,7 +64,7 @@ class Settings:
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up.
-        self.speedup_scale = 5.1
+        self.speedup_scale = 1.3
 
         # How quickly the alien point values increase.
         self.score_scale = 1.5
