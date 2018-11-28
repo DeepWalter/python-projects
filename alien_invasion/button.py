fa3b40e057e0bb92fc4ef2d339aa0fe5bd303a19
DeepWalter <deepwalter.cn@gmail.com>
2018-11-28 13:24:34 +0800

    Refactor and update docstring.

--- alien_invasion/button.py
+++ alien_invasion/button.py
@@ -3,34 +3,50 @@ import pygame.font
 
 
 class Button:
-    """The button class of filled rectangle with a label."""
+    """A class represents a button as filled rectangle with a label.
+
+    Methods
+    -------
+    draw_button()
+        Draw the button and its message on the screen
+    """
 
     def __init__(self, ai_settings, screen, msg):
-        """Initialize button attributes."""
-        self.screen = screen
-        self.screen_rect = self.screen.get_rect()
+        """Initialize button attributes.
+
+        Parameters
+        ----------
+        ai_settings: Settings
+            All settings for alien invasion
+        screen: pygame.Surface
+            The screen on which the button will be drawn
+        msg: str
+            The message displayed on the button
+        """
+        self._screen = screen
+        self._screen_rect = self._screen.get_rect()
 
         # Set the dimensions and properties of the button.
-        self.width, self.height = 200, 50
-        self.button_color = (169, 169, 169)
-        self.text_color = (255, 255, 255)
-        self.font = pygame.font.SysFont(None, 48)
+        self._width, self._height = 200, 50
+        self._button_color = (169, 169, 169)
+        self._text_color = (255, 255, 255)
+        self._font = pygame.font.SysFont(None, 48)
 
         # Build the button's rect object and center it.
-        self.rect = pygame.Rect(0, 0, self.width, self.height)
-        self.rect.center = self.screen_rect.center
+        self._rect = pygame.Rect(0, 0, self._width, self._height)
+        self._rect.center = self._screen_rect.center
 
         # The button message needs to be prepped only once.
-        self.prep_msg(msg)
+        self._prep_msg(msg)
 
-    def prep_msg(self, msg):
+    def _prep_msg(self, msg):
         """Turn msg into a rendered image and center text on the button."""
-        self.msg_image = self.font.render(msg, True, self.text_color,
-                                          self.button_color)
-        self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = self.rect.center
+        self._msg_image = self._font.render(msg, True, self._text_color,
+                                            self._button_color)
+        self._msg_image_rect = self._msg_image.get_rect()
+        self._msg_image_rect.center = self._rect.center
 
     def draw_button(self):
         """Draw the button and its message on the screen."""
-        self.screen.fill(self.button_color, self.rect)
-        self.screen.blit(self.msg_image, self.msg_image_rect)
+        self._screen.fill(self._button_color, self._rect)
+        self._screen.blit(self._msg_image, self._msg_image_rect)
