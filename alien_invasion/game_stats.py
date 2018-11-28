6dc553b756c5348b07d59a4d7c6a31a883de8b5d
DeepWalter <deepwalter.cn@gmail.com>
2018-11-28 11:28:53 +0800

    Update docstrings for GameStats class.

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,17 +1,44 @@
 class GameStats:
-    """Track statistics for alien invasion."""
+    """A class used to track statistics for alien invasion.
+
+    Attributes
+    ----------
+    ai_settings: Settings
+        All settings for alien invasion
+    game_active: bool
+        The status of the game
+    highest_score: int
+        The highest score in the game
+    ship_left: int
+        The number of available ships
+    score: int
+        Current score
+    level: int
+        Current level
+
+    Methods
+    -------
+    reset_stats()
+        Reset statistics when replay the game
+    """
 
     def __init__(self, ai_settings):
-        """Initialize statistics."""
+        """Initialize statistics.
+
+        Parameters
+        ----------
+        ai_settings: Settings
+            All settings for alien invasion
+        """
         self.ai_settings = ai_settings
         self.reset_stats()
-        # Start Alien Invasion in an active state.
+        # Start Alien Invasion in an inactive state.
         self.game_active = False
         # Highest score should never be reset.
         self.highest_score = 0
 
     def reset_stats(self):
-        """Initialize statistics that can change during the game."""
+        """Reset statistics when replay the game."""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
