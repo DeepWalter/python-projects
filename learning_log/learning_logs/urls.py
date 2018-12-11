a7abc1c7651eb0d239e893d3032b29ae361ddb02
DeepWalter <deepwalter.cn@gmail.com>
2018-12-11 23:12:00 +0800

    Support adding new entries.

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     # Show all topics.
     path('topics/', views.topics, name='topics'),
     path(r'topics/<int:topic_id>/', views.topic, name='topic'),
-    path('new_topic', views.new_topic, name='new_topic')
+    path('new_topic', views.new_topic, name='new_topic'),
+    path(r'new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
